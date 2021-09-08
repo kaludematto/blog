f7369572cdaeb096275e4545a1de2e545a31fc16
kaludematto <kalu.dematto@gmail.com>
2021-09-08 09:45:11 -0300

    Atualizacao blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/site', to: 'site#index'
+  root to: 'site#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
